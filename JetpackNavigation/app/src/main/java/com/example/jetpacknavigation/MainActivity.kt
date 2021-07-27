9a9ead042dbe319199fd875c6db0aed440deb251
ruthvik2 <ruthvik.b@spurtreetech.in>
2021-07-27 21:39:08 +0530

    Jetpack navigation - Drawer navigation

--- JetpackNavigation/app/src/main/java/com/example/jetpacknavigation/MainActivity.kt
+++ JetpackNavigation/app/src/main/java/com/example/jetpacknavigation/MainActivity.kt
@@ -3,22 +3,32 @@ package com.example.jetpacknavigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var homeBtn:TextView
+    private lateinit var aboutUsBtn:TextView
+    private lateinit var settingsBtn:TextView
+    private lateinit var profileBtn:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navController = findNavController(R.id.my_nav_host_fragment)
-
+        
         val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
@@ -34,11 +44,5 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_bottom_nav,menu)
         return true
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-
-    }
-
 
 }
