dc2af74daaf1151cda6b26b8068d6daf46d3a00a
ruthvik2 <ruthvik.b@spurtreetech.in>
2021-07-29 20:28:17 +0530

    Biometric login complete

--- BiometricLogin/app/src/main/java/com/example/biometriclogin/MainActivity.kt
+++ BiometricLogin/app/src/main/java/com/example/biometriclogin/MainActivity.kt
@@ -1,63 +1,84 @@
 package com.example.biometriclogin
 
-import android.content.Context
 import android.content.Intent
 import android.hardware.biometrics.BiometricManager
 import android.hardware.biometrics.BiometricPrompt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
-import com.example.biometriclogin.utils.CIPHERTEXT_WRAPPER
-import com.example.biometriclogin.utils.CryptographyManager
-import com.example.biometriclogin.utils.SHARED_PREFS_FILENAME
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG = "BIOMETRIC RESULT TAG"
     private lateinit var biometricPrompt: BiometricPrompt
-    private val cryptographyManager = CryptographyManager()
-    private val ciphertextWrapper
-        get() = cryptographyManager.getCiphertextWrapperFromSharedPrefs(
-            applicationContext,
-            SHARED_PREFS_FILENAME,
-            Context.MODE_PRIVATE,
-            CIPHERTEXT_WRAPPER
-        )
-
+    private lateinit var errorTv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val useBiometricsTv = findViewById<TextView>(R.id.useBiometrics)
-        val canAuthenticate = androidx.biometric.BiometricManager.from(applicationContext).canAuthenticate(
-            androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG )
+        val useBiometricsBtn = findViewById<Button>(R.id.button)
+        errorTv = findViewById(R.id.errorTv)
+        val canAuthenticate =
+            androidx.biometric.BiometricManager.from(applicationContext).canAuthenticate(
+                androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+            )
 
-        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS){
-            useBiometricsTv.visibility = View.VISIBLE
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS) {
 
-            useBiometricsTv.setOnClickListener {
-                if(ciphertextWrapper!=null){
-                    showBiometricPromptForDecryption()
-                }else{
-                    startActivity(Intent(this,EnableBiometricLoginActivity::class.java))
-                }
+            useBiometricsBtn.setOnClickListener {
+                showBiometricPrompt()
             }
-        } else{
-            useBiometricsTv.visibility = View.INVISIBLE
+        } else {
+            useBiometricsBtn.text = "Biometric login unavailable on your device"
         }
+    }
 
-        if (ciphertextWrapper == null) {
-            setupForLoginWithPassword()
+    private fun showBiometricPrompt() {
+        val biometricPrompt =
+           createBiometricPrompt(this)
+        val promptInfo = createPromptInfo(this)
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun navigate(){
+        startActivity(Intent(this,HomeActivity::class.java))
+    }
+
+    private fun createBiometricPrompt(
+        activity: AppCompatActivity,
+
+        ): androidx.biometric.BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(activity)
+        val callback = object : androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+
+                errorTv.visibility = View.VISIBLE
+            }
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                errorTv.visibility = View.VISIBLE
+                errorTv.text = "User rejected login"
+            }
+            override fun onAuthenticationSucceeded(result: androidx.biometric.BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                errorTv.visibility = View.GONE
+                navigate()
+            }
         }
-
+        return androidx.biometric.BiometricPrompt(activity, executor, callback)
     }
 
+    private fun createPromptInfo(activity: AppCompatActivity): androidx.biometric.BiometricPrompt.PromptInfo =
+        androidx.biometric.BiometricPrompt.PromptInfo.Builder().apply {
+            setTitle(activity.getString(R.string.prompt_info_title))
+            setConfirmationRequired(false)
+            setNegativeButtonText("Cancel")
+        }.build()
 
-    private fun setupForLoginWithPassword() {
-        TODO("Not yet implemented")
-    }
 
 
-    private fun showBiometricPromptForDecryption() {
-        TODO("Not yet implemented")
-    }
 }
--- BiometricLogin/app/src/main/java/com/example/biometriclogin/utils/Constants.kt
+++
@@ -1,4 +0,0 @@
-package com.example.biometriclogin.utils
-
-const val SHARED_PREFS_FILENAME = "biometric_prefs"
-const val CIPHERTEXT_WRAPPER = "ciphertext_wrapper"
